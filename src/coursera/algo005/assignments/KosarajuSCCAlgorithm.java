04c2c67f0c64e2ca8768f7f48ce85d2be3456186
samer-meggaly <samer.samy.nazzir@gmail.com>
2014-07-04 17:38:14 +0300

    Reformat of source code

--- src/coursera/algo005/assignments/KosarajuSCCAlgorithm.java
+++ src/coursera/algo005/assignments/KosarajuSCCAlgorithm.java
@@ -20,25 +20,27 @@ import java.util.Set;
 import java.util.Stack;
 
 /**
- * This algorithm has a bug in it has I haven't found yet.
+ * This algorithm has a bug in it has I haven't found yet. 
  * [FIXED] The bug was in the computation of the finishing time. :)
+ * 
  * @author samer
- *
+ * 
  */
 public class KosarajuSCCAlgorithm {
-	
+
 	private static final boolean _RUN_REC_DFS = false;
-	
+
 	private static final int _NOT_VISITED = -2;
 	private static final int _VISITED = 0;
-	
+
 	private GraphDirected dg;
 	private Map<Integer, Integer> vertexFinishingTime;
 	private Map<Integer, Integer> vertexLeader;
-	
+
 	public KosarajuSCCAlgorithm(GraphDirected dag) {
 		this.dg = dag;
-		vertexFinishingTime = new HashMap<Integer, Integer>(dag.getNumOfVertices());
+		vertexFinishingTime = new HashMap<Integer, Integer>(
+				dag.getNumOfVertices());
 		for (Integer vid : dag.getVerticesIDs()) {
 			vertexFinishingTime.put(vid, _NOT_VISITED);
 		}
@@ -47,15 +49,15 @@ public class KosarajuSCCAlgorithm {
 			vertexLeader.put(vid, _NOT_VISITED);
 		}
 	}
-	
+
 	public Map<Integer, Integer> getVerticesFinishingTimes() {
 		return this.vertexFinishingTime;
 	}
-	
+
 	public Map<Integer, Integer> getVerticesLeaders() {
 		return this.vertexLeader;
 	}
-	
+
 	public Map<Integer, Set<Integer>> extractSCCWithLeaders() {
 		System.out
 				.println("Computing the vertices finishing time in the reverse DAG");
@@ -69,11 +71,11 @@ public class KosarajuSCCAlgorithm {
 					recursiveDFS(dgRev, vid);
 			}
 		}
-		
+
 		System.out
 				.println("Sorting the vertices finishing times in descending order");
 		extractSCCFromOriginalDG();
-		
+
 		System.out
 				.println("Aggregating each SCC vertices by common leader vertex");
 		Map<Integer, Set<Integer>> sccs = new HashMap<Integer, Set<Integer>>();
@@ -88,12 +90,13 @@ public class KosarajuSCCAlgorithm {
 			}
 			scc.add(e.getKey());
 		}
-		
+
 		return sccs;
 	}
-	
+
 	/**
-	 * This function computes the finishing time of every vertex in the reversed directed-graph.
+	 * This function computes the finishing time of every vertex in the reversed
+	 * directed-graph.
 	 */
 	private void computeRevDGVerticesFinishingTime() {
 		GraphDirected revDG = dg.getReversed();
@@ -101,7 +104,7 @@ public class KosarajuSCCAlgorithm {
 		List<Integer> verticesIDs = revDG.getVerticesIDs();
 		for (Integer vid : verticesIDs) {
 			if (vertexFinishingTime.get(vid) == _NOT_VISITED) {
-				
+
 				// invoke DSF starting from current vertex
 				Stack<Integer> stack = new Stack<Integer>();
 				stack.push(vid);
@@ -111,7 +114,8 @@ public class KosarajuSCCAlgorithm {
 						vertexFinishingTime.put(currVID, _VISITED);
 						stack.push(currVID);
 						Vertex currV = revDG.getVertexByID(currVID);
-						for (Integer currVAdjID : currV.getDistinctAdjVertices()) {
+						for (Integer currVAdjID : currV
+								.getDistinctAdjVertices()) {
 							if (vertexFinishingTime.get(currVAdjID) == _NOT_VISITED) {
 								stack.push(currVAdjID);
 							}
@@ -119,12 +123,13 @@ public class KosarajuSCCAlgorithm {
 					} else if (vertexFinishingTime.get(currVID) == _VISITED) {
 						vertexFinishingTime.put(currVID, ++ft);
 					}
-				}								
+				}
 			}
-		}		
+		}
 	}
-	
+
 	private static int t = 1;
+
 	private void recursiveDFS(GraphDirected dg, int vid) {
 		vertexFinishingTime.put(vid, _VISITED);
 		List<Integer> adj = new ArrayList<Integer>(dg.getVertexByID(vid)
@@ -135,22 +140,22 @@ public class KosarajuSCCAlgorithm {
 				recursiveDFS(dg, uid);
 			}
 		}
-		
+
 		vertexFinishingTime.put(vid, t++);
 	}
-	
+
 	private void extractSCCFromOriginalDG() {
 		List<Entry<Integer, Integer>> vertFTList = new ArrayList<Entry<Integer, Integer>>(
 				vertexFinishingTime.entrySet());
 		Collections.sort(vertFTList, new Comparator<Entry<Integer, Integer>>() {
-			
+
 			@Override
 			public int compare(Entry<Integer, Integer> o1,
 					Entry<Integer, Integer> o2) {
 				return o2.getValue() - o1.getValue();
 			}
-		});	
-		
+		});
+
 		for (Entry<Integer, Integer> e : vertFTList) {
 			Integer vid = e.getKey();
 			if (vertexLeader.get(vid) == _NOT_VISITED) {
@@ -177,7 +182,7 @@ public class KosarajuSCCAlgorithm {
 			}
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		int N = 20;
 		int n = 13;
@@ -203,12 +208,13 @@ public class KosarajuSCCAlgorithm {
 				sb.append("0 ");
 		}
 		System.out.println(sb);
-		
+
 		System.out.println(ft);
 		System.out.println(ls);
 	}
-	
-	public static GraphDirected readGraphFromFile(String filePath, int numOfVertices) {
+
+	public static GraphDirected readGraphFromFile(String filePath,
+			int numOfVertices) {
 		try {
 			Scanner scanner = new Scanner(new BufferedInputStream(
 					new FileInputStream(new File(filePath))));
